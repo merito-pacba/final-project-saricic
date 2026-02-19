1b5c66d2f3670d3e86175b68794a22d3629ab473
SERKAN SARIÇİÇEK <152647447+saricic@users.noreply.github.com>
2026-02-19 12:24:31 +0000

    search bar added

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -33,6 +33,11 @@ class RegisterPage(FormView):
             login(self.request, user)
         return super(RegisterPage,self).form_valid(form)
 
+    def get (self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args,**kwargs)
+
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
@@ -42,6 +47,12 @@ class TaskList(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__startswith = search_input)
+
+        context['search_input'] = search_input
         return context
                             
 class TaskDetail(LoginRequiredMixin, DetailView):
